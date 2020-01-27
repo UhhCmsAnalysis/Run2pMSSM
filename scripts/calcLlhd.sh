601152e1e0984e072a1f84a4628b51bdfd75b945
Sam Bein <samuel.bein@gmail.com>
2020-01-27 23:47:41 +0100

    edited out wrong txt file names

--- scripts/calcLlhd.sh
+++ scripts/calcLlhd.sh
@@ -11,13 +11,13 @@ python scripts/calcLlhdMultiCount.py idata/SUS12011/counts.sezen.txt idata/SUS12
 
 #max llhd is probably the nicest result we can have:
 python scripts/calcLlhdMaxLlhd.py idata/SUS12024/maxllhd.txt idata/SUS12024/llhd.txt
-python scripts/calcLlhdMultiCount.py idata/SUS13012/counts.sezen.txt idata/SUS13012/data_bg.txt idata/SUS13012/llhd.txt
+python scripts/calcLlhdMultiCount.py idata/SUS13012/counts.txt idata/SUS13012/data_bg.txt idata/SUS13012/llhd.txt
 
 ########
 # 13 TeV
 ########
 
-python scripts/calcLlhdMultiCount.py idata/SUS16033/counts.sezen.txt idata/SUS16033/data_bg.txt idata/SUS16033/llhd.txt
+python scripts/calcLlhdMultiCount.py idata/SUS16033/counts.txt idata/SUS16033/data_bg.txt idata/SUS16033/llhd.txt
 
 ########
 # 7+8+13
